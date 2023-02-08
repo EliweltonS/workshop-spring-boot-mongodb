60a24940ee7e34634e6da925a450741b02cdec2e
Eliwelton <eliwelton.rc@gmail.com>
2023-02-08 19:17:11 -0300

    Obtendo um usuário por id

--- src/main/java/com/eliwelton/workshopmongo/services/UserService.java
+++ src/main/java/com/eliwelton/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.eliwelton.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eliwelton.workshopmongo.domain.User;
 import com.eliwelton.workshopmongo.repository.UserRepository;
+import com.eliwelton.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
-	private UserRepository repo;//ja instanciado "injeção de dependencia automatica do spring"
-	
-	public List<User> findAll(){
+	private UserRepository repo;// ja instanciado "injeção de dependencia automatica do spring"
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
