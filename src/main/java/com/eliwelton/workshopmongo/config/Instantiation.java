fa61524bb397926b3399d30bead52ec053bd2ec7
Eliwelton <eliwelton.rc@gmail.com>
2023-02-08 20:44:47 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/eliwelton/workshopmongo/config/Instantiation.java
+++ src/main/java/com/eliwelton/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.eliwelton.workshopmongo.domain.Post;
 import com.eliwelton.workshopmongo.domain.User;
 import com.eliwelton.workshopmongo.dto.AuthorDTO;
+import com.eliwelton.workshopmongo.dto.CommentDTO;
 import com.eliwelton.workshopmongo.repository.PostRepository;
 import com.eliwelton.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "To indo pra SP, Valeu", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem amigão", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite :-)", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um Otimo dia :-)", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
