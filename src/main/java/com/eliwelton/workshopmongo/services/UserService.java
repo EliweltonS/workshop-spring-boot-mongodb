332a9a278f30c48c59e343a6fec7b93a4dcaad94
Eliwelton <eliwelton.rc@gmail.com>
2023-02-08 19:33:56 -0300

    Inserção de usuário com POST

--- src/main/java/com/eliwelton/workshopmongo/services/UserService.java
+++ src/main/java/com/eliwelton/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eliwelton.workshopmongo.domain.User;
+import com.eliwelton.workshopmongo.dto.UserDTO;
 import com.eliwelton.workshopmongo.repository.UserRepository;
 import com.eliwelton.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	
+//implmentando o FROMDTO aqui porque : caso seja necessario uma operação para manuteção futura que ja tem acesso a dados
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
