bb8c5ebe78389da1ca4f7f4ddabff99cd6804e28
Eliwelton <eliwelton.rc@gmail.com>
2023-02-08 20:08:26 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/eliwelton/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.eliwelton.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eliwelton.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+}
