f361a62ca4572d7c8b7c7c50146bef994756a76b
Eliwelton <eliwelton.rc@gmail.com>
2023-02-10 13:09:48 -0300

    Consulta simples com query methods

--- src/main/java/com/eliwelton/workshopmongo/services/PostService.java
+++ src/main/java/com/eliwelton/workshopmongo/services/PostService.java
@@ -1,13 +1,12 @@
 package com.eliwelton.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eliwelton.workshopmongo.domain.Post;
-import com.eliwelton.workshopmongo.domain.User;
-import com.eliwelton.workshopmongo.dto.UserDTO;
 import com.eliwelton.workshopmongo.repository.PostRepository;
 import com.eliwelton.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,16 +14,14 @@ import com.eliwelton.workshopmongo.services.exception.ObjectNotFoundException;
 public class PostService {
 
 	@Autowired
-	private PostRepository repo;// ja instanciado "injeção de dependencia automatica do spring"
-
-	public Post findById(String id) {
-	    Optional<Post> user = repo.findById(id);
-	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
-	}
-
-	// implmentando o FROMDTO aqui porque : caso seja necessario uma operação para
-	// manuteção futura que ja tem acesso a dados
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	private PostRepository repo;
+	
+	public Post  findById(String id) {
+		Optional<Post > obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
